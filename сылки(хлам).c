9d6b09727918449216152957f3e478b1435fd8f6
Fantom2039 <154364699+Fantom2039@users.noreply.github.com>
2023-12-19 21:06:56 +0300

    Add files via upload

---
+++ сылки(хлам).c
@@ -0,0 +1,9 @@
+//https://ru.wikipedia.org/wiki/%D0%9E%D0%B4%D0%B5%D0%B6%D0%B4%D0%B0#%D0%9E%D1%81%D0%BD%D0%BE%D0%B2%D0%BD%D1%8B%D0%B5_%D0%BF%D0%BE%D0%BD%D1%8F%D1%82%D0%B8%D1%8F
+//
+//
+//
+//
+//
+//
+//
+//
